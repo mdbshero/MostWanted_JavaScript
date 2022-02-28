5a71a292e1461b8ed51c98af1c3134787f7db702
Mdbshero <mdbshero@gmail.com>
2022-02-28 17:14:00 -0600

    Edited Info Funtionality and Formatting.

--- app.js
+++ app.js
@@ -9,7 +9,7 @@
 // app is the function called to start the entire application
 function app(people) {
   let searchType = promptFor("Do you know the name of the person you are looking for? Enter 'yes' or 'no'", yesNo).toLowerCase();
-  let searchResults = {}
+  let searchResults = {};
   switch (searchType) {
     case 'yes':
       searchResults = searchByName(people);
@@ -51,7 +51,7 @@ function mainMenu(person, people) {
       // }
       // alert(printThis(person))
 
-      alert(JSON.stringify(person, null, 1).replace(/({|}|\[|\]|"|\n,)/g, ''))
+      displayPerson(person);
       mainMenu(person, people); // restart
     break;
     case "family":
@@ -80,28 +80,28 @@ function mainMenu(person, people) {
 
 function familyFinder(person, people){
   let parents = JSON.stringify(people.filter(family=>person[0].parents.includes((family.id))),['lastName','firstName'],1)
-  .replace(/({|}|\[|\]|")/g, '').replace(/(firstName: |lastName: )/g,'').replace(/(\n |\n)/g, '')
+  .replace(/({|}|\[|\]|")/g, '').replace(/(firstName: |lastName: )/g,'').replace(/(\n |\n)/g, '');
 
   let spouse = (JSON.stringify(((people.filter(family=>person[0].currentSpouse == family.id)) != ''? people.filter(family=>person[0].currentSpouse == family.id):"N/A"),['firstName','lastName'],1))
-  .replace(/({|}|\[|\]|"|,)/g, '').replace(/(firstName: |lastName: )/g,'').replace(/(\n,|,\n |\n |\n)/g, '')
+  .replace(/({|}|\[|\]|"|,)/g, '').replace(/(firstName: |lastName: )/g,'').replace(/(\n,|,\n |\n |\n)/g, '');
  
   // let spouse = (JSON.stringify(people.filter(family=>person[0].currentSpouse == family.id) != ''? people.filter(family=>person[0].currentSpouse == family.id):"N/A"),['firstName','lastName'],1).replace(/({|}|\[|\]|"|,)/g, '').replace(/(firstName: |lastName: )/g,'').replace(/(\n,|,\n |\n |\n)/g, '')
   let siblings = (JSON.stringify(((people.filter(family=>(family.parents.includes(person[0].parents[0]) | family.parents.includes(person[0].parents[1])) && family != person[0])) != ''?people.filter(family=>(family.parents.includes(person[0].parents[0]) | family.parents.includes(person[0].parents[1])) && family != person[0]) :"N/A"),['firstName','lastName'],1))
-  .replace(/({|}|\[|\]|"|,)/g, '').replace(/(firstName: |lastName: )/g,'').replace(/(\n,|,\n |\n |\n)/g, '')
+  .replace(/({|}|\[|\]|"|,)/g, '').replace(/(firstName: |lastName: )/g,'').replace(/(\n,|,\n |\n |\n)/g, '');
 
   // let spouse = people.filter(family=>person[0].currentSpouse == family.id)
-  let stringReturn = `Family of ${person[0].firstName} ${person[0].lastName} ID: ${person[0].id}\nParent(s): ${parents}\nSpouse:${spouse}\nSiblings:${siblings}`
-  return(stringReturn)
+  let stringReturn = `Family of ${person[0].firstName} ${person[0].lastName} ID: ${person[0].id}\nParent(s): ${parents}\nSpouse:${spouse}\nSiblings:${siblings}`;
+  return(stringReturn);
 }
 
 function descendantFinder(person, people){
   let descendants = JSON.stringify(((people.filter(offspring => offspring.parents.includes(person[0].id))) != ''?people.filter(offspring => offspring.parents.includes(person[0].id)):"N/A"),['firstName','lastName'],1)
-  .replace(/({|}|\[|\]|"|,)/g, '').replace(/(firstName: |lastName: )/g,'').replace(/(\n,|,\n |\n |\n)/g, '')
+  .replace(/({|}|\[|\]|"|,)/g, '').replace(/(firstName: |lastName: )/g,'').replace(/(\n,|,\n |\n |\n)/g, '');
 
   //!= ''? people.filter(offspring.parents.includes((person[0].id)):"N/A"),['firstName','lastName'],1).replace(/({|}|\[|\]|"|\n,)/g, '').replace(/(firstName: |lastName: )/g,'').replace(/(\n,|,\n )/g, '')
   //let spouse = (JSON.stringify(((people.filter(family=>person[0].currentSpouse == family.id)) != ''? people.filter(family=>person[0].currentSpouse == family.id):"N/A"),['firstName','lastName'],1)).replace(/({|}|\[|\]|"|,)/g, '').replace(/(firstName: |lastName: )/g,'').replace(/(\n,|,\n )/g, '')
-  let stringReturn = `Descendants of ${person[0].firstName} ${person[0].lastName} ID: ${person[0].id}\nDecendant(s): ${descendants}`
-  return(stringReturn)
+  let stringReturn = `Descendants of ${person[0].firstName} ${person[0].lastName} ID: ${person[0].id}\nDecendant(s): ${descendants}`;
+  return(stringReturn);
 }
 
 
@@ -117,7 +117,7 @@ function searchByName(people) {
   let firstName = promptFor("What is the person's first name?", autoValid).toLowerCase();
   let lastName = promptFor("What is the person's last name?", autoValid).toLowerCase();
 
-  let foundPerson = people.filter(potentialMatch => potentialMatch.firstName.toLowerCase() === firstName && potentialMatch.lastName.toLowerCase() === lastName)
+  let foundPerson = people.filter(potentialMatch => potentialMatch.firstName.toLowerCase() === firstName && potentialMatch.lastName.toLowerCase() === lastName);
 
 
   // TODO: find the person single person object using the name they entered.
@@ -129,32 +129,32 @@ function searchByTrait(people){
   let traitsToSearch = promptFor(`Please enter the traits you would like to search for followed by a colon.\nSeparate queries by AND or a Comma (,)\nExample: gender:male,eye color:blue AND occupation:nurse`,autoValid)
   // traitsToSearch = traitsToSearch.split(/(AND|,)/g)
   // let newReg = /( AND |\,| , |, | ,|AND | AND)/g
-  traitsToSearch = traitsToSearch.split(/ AND | , |, | ,|,/g)
+  traitsToSearch = traitsToSearch.split(/ AND | , |, | ,|,/g);
   let peopleWithTraits = people;
 
   if (traitsToSearch.filter(person=> /gender/gi.test(person)).length != 0){
-    peopleWithTraits = searchByGender(peopleWithTraits,traitsToSearch.filter(person=> /gender/gi.test(person)))
+    peopleWithTraits = searchByGender(peopleWithTraits,traitsToSearch.filter(person=> /gender/gi.test(person)));
     // people = searchByGender(people, traitsToSearch)
   } 
   if (traitsToSearch.filter(person=> /occupation/gi.test(person)).length != 0){
-    peopleWithTraits = searchByOccupation(peopleWithTraits,traitsToSearch.filter(person=> /occupation/gi.test(person)))
+    peopleWithTraits = searchByOccupation(peopleWithTraits,traitsToSearch.filter(person=> /occupation/gi.test(person)));
     
     // people = searchByGender(people, traitsToSearch)
   }
   if (traitsToSearch.filter(person=> /eyecolor/gi.test(person)).length != 0){
-    peopleWithTraits = searchByEyeColor(peopleWithTraits,traitsToSearch.filter(person=> /eyecolor/gi.test(person)))
+    peopleWithTraits = searchByEyeColor(peopleWithTraits,traitsToSearch.filter(person=> /eyecolor/gi.test(person)));
     // people = searchByEyeColor(people, traitsToSearch)
   }
   if (traitsToSearch.filter(person=> /weight/gi.test(person)).length != 0){
-    peopleWithTraits = searchByWeight(peopleWithTraits,traitsToSearch.filter(person=> /weight/gi.test(person)))
+    peopleWithTraits = searchByWeight(peopleWithTraits,traitsToSearch.filter(person=> /weight/gi.test(person)));
     // people = searchByEyeColor(people, traitsToSearch)
   }
   if (traitsToSearch.filter(person=> /height/gi.test(person)).length != 0){
-    peopleWithTraits = searchByHeight(peopleWithTraits, traitsToSearch.filter(person=> /height/gi.test(person)))
+    peopleWithTraits = searchByHeight(peopleWithTraits, traitsToSearch.filter(person=> /height/gi.test(person)));
   }
 
-  displayPeople(peopleWithTraits)
-  app(people)
+  displayPeople(peopleWithTraits);
+  app(people);
 }
 
 
@@ -163,23 +163,23 @@ function searchByTrait(people){
 function searchByGender(people, traitsToSearch) {
   console.log(traitsToSearch)
   let searchedGender = traitsToSearch[0].split(":");
-  return(people.filter(person => person.gender == searchedGender[1]))
+  return(people.filter(person => person.gender == searchedGender[1]));
 }
 function searchByOccupation(people,traitsToSearch){
-  let newTraitsToSearch = traitsToSearch[0].split(":")
-  return(people.filter(person=>person.occupation == newTraitsToSearch[1]))
+  let newTraitsToSearch = traitsToSearch[0].split(":");
+  return(people.filter(person=>person.occupation == newTraitsToSearch[1]));
 }
 function searchByEyeColor(people,traitsToSearch){
-  let newTraitsToSearch = traitsToSearch[0].split(":")
-  return(people.filter(person=>person.eyeColor==newTraitsToSearch[1]))
+  let newTraitsToSearch = traitsToSearch[0].split(":");
+  return(people.filter(person=>person.eyeColor==newTraitsToSearch[1]));
 }
 function searchByWeight(people,traitsToSearch){
-  let newTraitsToSearch = traitsToSearch[0].split(":")
-  return(people.filter(person=>person.weight==newTraitsToSearch[1]))
+  let newTraitsToSearch = traitsToSearch[0].split(":");
+  return(people.filter(person=>person.weight==newTraitsToSearch[1]));
 }
 function searchByHeight(people,traitsToSearch){
-  let newTraitsToSearch = traitsToSearch[0].split(":")
-  return(people.filter(person=>person.height==newTraitsToSearch[1]))
+  let newTraitsToSearch = traitsToSearch[0].split(":");
+  return(people.filter(person=>person.height==newTraitsToSearch[1]));
 }
 //TODO: add other trait filter functions here.
 //Traits to search by:EyeColor, Height, Weight, Occupation, Gender
@@ -202,8 +202,16 @@ function displayPeople(people) {
 function displayPerson(person) {
   // print all of the information about a person:
   // height, weight, age, name, occupation, eye color.
-  let personInfo = "First Name: " + person.firstName + "\n";
-  personInfo += "Last Name: " + person.lastName + "\n";
+  let personInfo = "First Name: " + person[0].firstName + "\n";
+  personInfo += "Last Name: " + person[0].lastName + "\n";
+  personInfo += "Gender: " + person[0].gender + "\n";
+  personInfo += "DOB: " + person[0].dob + "\n";
+  personInfo += "Height: " + person[0].height + "\n";
+  personInfo += "Weight: " + person[0].weight + "\n";
+  personInfo += "Eye Color: " + person[0].eyeColor + "\n";
+  personInfo += "Occupation: " + person[0].occupation +"\n";
+  personInfo += "Parents: " + person[0].parents + "\n";
+  personInfo += "Current Spouse: " + person[0].currentSpouse + "\n";
   // TODO: finish getting the rest of the information to display.
   alert(personInfo);
 }
